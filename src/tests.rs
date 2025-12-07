5b5e0ecbf8cbd10297ef29906254a890d3ce4500
stable-release <stable-release@users.noreply.github.com>
2025-12-07 21:42:35 +0800

    Cleanup tests.rs

--- src/tests.rs
+++ src/tests.rs
@@ -1,19 +1,4 @@
 #[cfg(test)]
 mod tests {
-    // use crate::store::memory::{Command, parse_arguments};
-
-    // use super::*;
-
-    // #[test]
-    // fn it_works() {
-    //     let result = add(2, 2);
-    //     assert_eq!(result, 4);
-    // }
-
-    // 1. set
-    // 2. get
-    // 3. list
-    // 4. delete
-    // 5. clear
-    // 6. exit
+    // Tests are located in-file
 }
